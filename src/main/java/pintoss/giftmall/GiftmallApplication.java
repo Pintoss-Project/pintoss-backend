af95997f48bdb28a6499289e2d624bf4d3678c78
DataCodeLiteracy <dataliteracy@icloud.com>
2024-07-16 16:53:51 +0900

    [ ⚙️ | | env ] 스프링부트 스타터로 프로젝트 초기 설정
    2024.07.16

---
+++ src/main/java/pintoss/giftmall/GiftmallApplication.java
@@ -0,0 +1,13 @@
+package pintoss.giftmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pintoss/giftmall/GiftmallApplicationTests.java
@@ -0,0 +1,13 @@
+package pintoss.giftmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
