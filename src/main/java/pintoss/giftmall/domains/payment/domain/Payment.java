48c8356e77b5ede3072988f99befc773d3e45d56
DataCodeLiteracy <dataliteracy@icloud.com>
2024-07-19 17:38:21 +0900

    [ ✨ | #1 | feature ] 결제 엔티티 구현
    2024.07.19

---
+++ src/main/java/pintoss/giftmall/domains/payment/domain/Payment.java
@@ -0,0 +1,32 @@
+package pintoss.giftmall.domains.payment.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_id")
+    private Long id;
+
+    @Column(length = 10)
+    private String payStatus;
+
+    @Column(length = 10)
+    private String payMethod;
+
+    private int totalPrice;
+    private int discountPrice;
+
+    @CreatedDate
+    private LocalDateTime approvedAt;
+
+}
