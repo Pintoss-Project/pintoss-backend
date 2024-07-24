4120f506cfb5e0328312367d81e8b16940a9a075
DataCodeLiteracy <dataliteracy@icloud.com>
2024-07-24 17:26:44 +0900

    [ ♻️  | #5 | refactor ] 피드백 받아서 컨트롤러 admin 부분 분할
    2024.07.24

---
+++ src/main/java/pintoss/giftmall/domains/site_info/controller/SiteInfoAdminController.java
@@ -0,0 +1,23 @@
+package pintoss.giftmall.domains.site_info.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pintoss.giftmall.domains.site_info.dto.SiteInfoResponseDTO;
+import pintoss.giftmall.domains.site_info.dto.SiteInfoUpdateRequestDTO;
+import pintoss.giftmall.domains.site_info.service.SiteInfoService;
+
+@RestController
+@RequestMapping("/api/admin/site-info")
+@RequiredArgsConstructor
+public class SiteInfoAdminController {
+
+    private final SiteInfoService siteInfoService;
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<SiteInfoResponseDTO> updateSiteInfo(@PathVariable Long id, @RequestBody SiteInfoUpdateRequestDTO requestDTO) {
+        SiteInfoResponseDTO updateSiteInfo = siteInfoService.update(id, requestDTO);
+        return ResponseEntity.ok(updateSiteInfo);
+    }
+
+}
--- src/main/java/pintoss/giftmall/domains/site_info/controller/SiteInfoController.java
+++ src/main/java/pintoss/giftmall/domains/site_info/controller/SiteInfoController.java
@@ -4,34 +4,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pintoss.giftmall.domains.site_info.dto.SiteInfoResponseDTO;
-import pintoss.giftmall.domains.site_info.dto.SiteInfoUpdateRequestDTO;
 import pintoss.giftmall.domains.site_info.service.SiteInfoService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/site-info")
 @RequiredArgsConstructor
 public class SiteInfoController {
 
     private final SiteInfoService siteInfoService;
 
-    @GetMapping("/site-info")
     public ResponseEntity<List<SiteInfoResponseDTO>> getAllSiteInfo() {
         List<SiteInfoResponseDTO> siteInfoList = siteInfoService.findAll();
         return ResponseEntity.ok(siteInfoList);
     }
 
-    @GetMapping("/site-info/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<SiteInfoResponseDTO> getSiteInfoById(@PathVariable Long id) {
         SiteInfoResponseDTO siteInfo = siteInfoService.findById(id);
         return ResponseEntity.ok(siteInfo);
-    }
-
-    @PatchMapping("/admin/site-info/{id}")
-    public ResponseEntity<SiteInfoResponseDTO> updateSiteInfo(@PathVariable Long id, @RequestBody SiteInfoUpdateRequestDTO requestDTO) {
-        SiteInfoResponseDTO updateSiteInfo = siteInfoService.update(id, requestDTO);
-        return ResponseEntity.ok(updateSiteInfo);
     }
 
 }
