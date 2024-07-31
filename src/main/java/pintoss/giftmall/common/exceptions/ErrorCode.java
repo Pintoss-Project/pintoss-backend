fc4e364d3e6a387e216f53358429a5a343b0fa22
DataCodeLiteracy <dataliteracy@icloud.com>
2024-07-31 20:27:13 +0900

    [ ♻️  | #20 | refactor ] ErrorCode 메세지 수정
    2024.07.31

--- src/main/java/pintoss/giftmall/common/exceptions/ErrorCode.java
+++ src/main/java/pintoss/giftmall/common/exceptions/ErrorCode.java
@@ -8,11 +8,10 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
 
     NOT_FOUND("자원을 찾을 수 없습니다."),
-    NOT_FOUND_ID("자원을 찾을 수 없습니다. ID : "),
     INVALID_REQUEST("유효하지 않은 요청입니다."),
-    CREATION_FAILURE("생성 실패"),
-    UPDATE_FAILURE("업데이트 실패"),
-    DELETION_FAILURE("삭제 실패");
+    CREATION_FAILURE("생성에 실패했습니다."),
+    UPDATE_FAILURE("업데이트에 실패했습니다."),
+    DELETION_FAILURE("삭제에 실패했습니다.");
 
     private final String message;
 
