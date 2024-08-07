41d734100d7bd295a9379f301ee63a69b83be1c5
DataCodeLiteracy <dataliteracy@icloud.com>
2024-08-07 18:52:55 +0900

    [ ⚙️ | #31 | env ] jar -> war 설정 변경
    2024.08.07

--- src/main/java/pintoss/giftmall/GiftmallApplication.java
+++ src/main/java/pintoss/giftmall/GiftmallApplication.java
@@ -2,14 +2,21 @@ package pintoss.giftmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class GiftmallApplication {
+public class GiftmallApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GiftmallApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GiftmallApplication.class);
 	}
 
 }
