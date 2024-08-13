e7ee14ae42eb656b34867bccba23c8c57378811e
DataCodeLiteracy <dataliteracy@icloud.com>
2024-08-13 18:58:54 +0900

    [ ♻️  | #38 | refactor ] 토큰 헤더에 전달하도록 수정
    2024.08.13

--- src/main/java/pintoss/giftmall/domains/user/controller/UserController.java
+++ src/main/java/pintoss/giftmall/domains/user/controller/UserController.java
@@ -3,10 +3,7 @@ package pintoss.giftmall.domains.user.controller;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pintoss.giftmall.common.responseobj.ApiResponse;
 import pintoss.giftmall.domains.user.dto.UserResponse;
 import pintoss.giftmall.domains.user.service.UserService;
@@ -19,8 +16,9 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/user_info")
-    public ApiResponse<UserResponse> getUser(@RequestBody @NotBlank String token) {
-        UserResponse user = userService.getUserInfo(token);
+    public ApiResponse<UserResponse> getUser(@RequestHeader("Authorization") @NotBlank String token) {
+        String cleanedToken = token.replace("bearer ", "");
+        UserResponse user = userService.getUserInfo(cleanedToken);
         return ApiResponse.ok(user);
     }
 
